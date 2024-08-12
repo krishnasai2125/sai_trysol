7993c76445d6e91e1cef7be8002904facab39958
krishnasai2125 <151649679+krishnasai2125@users.noreply.github.com>
2024-08-12 18:14:36 +0530

    Update varibale.tf

--- varibale.tf
+++ varibale.tf
@@ -1,17 +1,1 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  default     = "AKIA2UC3ASYFRXPMBXUQ"
-}
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  default     = "H/qQquBxfpu8GM5wSaDkMOVe+4KRjqk+U+gvni1S"
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
