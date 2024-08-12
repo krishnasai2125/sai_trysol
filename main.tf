6dfb2b5e3f606fb5cca5038e28d7a39e5e3704b7
krishnasai2125 <151649679+krishnasai2125@users.noreply.github.com>
2024-08-12 13:03:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 resource "aws_instance" "example" {
   ami           = "ami-0a4408457f9a03be3"
-  AWS_ACCESS_KEY_ID = credentials('AKIA2UC3ASYFRXPMBXUQ')  
- AWS_SECRET_ACCESS_KEY = credentials('H/qQquBxfpu8GM5wSaDkMOVe+4KRjqk+U+gvni1S')  
+  AWS_ACCESS_KEY_ID = "credentials('AKIA2UC3ASYFRXPMBXUQ')"
+ AWS_SECRET_ACCESS_KEY = "credentials('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')"  
 instance_type = "t2.medium"
   key_name      = "i12" 
   tags = {
