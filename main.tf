fa5314ddfc837855bc87724d11bb21240a7eb8ea
krishnasai2125 <151649679+krishnasai2125@users.noreply.github.com>
2024-08-12 13:06:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0a4408457f9a03be3"
-  AWS_ACCESS_KEY_ID = "credentials('AKIA2UC3ASYFRXPMBXUQ')"
- AWS_SECRET_ACCESS_KEY = "credentials('H/qQquBxfpu8GM5wSaDkMOVe+4KRjqk+U+gvni1S')"  
+  ami           = "ami-0a4408457f9a03be3
 instance_type = "t2.medium"
   key_name      = "i12" 
   tags = {
