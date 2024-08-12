27f8113e1907db121b7f55fcd527b0453d7decd4
krishnasai2125 <151649679+krishnasai2125@users.noreply.github.com>
2024-08-12 14:09:17 +0530

    Update varibale.tf

--- varibale.tf
+++ varibale.tf
@@ -1,4 +1,17 @@
- environment {
-        AWS_ACCESS_KEY_ID = credentials('AKIA2UC3ASYFRXPMBXUQ')  
-        AWS_SECRET_ACCESS_KEY = credentials('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')  
-    }
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "AKIA2UC3ASYFRXPMBXUQ"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = "H/qQquBxfpu8GM5wSaDkMOVe+4KRjqk+U+gvni1S"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
